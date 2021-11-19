445feb018d6d662b2111eacbe48f4d2575e13704
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2021-11-19 08:38:51 +0700

    Create sapxepchen.c

---
+++ sapxep/sapxepchen.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void insertionSort(int A[],int n){
+	int i,j,last;
+	for(i=1;i<n;i++){
+		last = A[i];
+		j = i;
+		while((j>1) && (A[j-1]>last)){
+			A[j] = A[j-1];
+			j--;
+		}
+		A[j] = last;
+	}
+}
+//tot nhat: 0 hoan doi, n-1 so sanh
+//toi nhat: n(n-1)/2 hoan doi va so sanh
+//trung binh: n(n-1)/4 hoan doi va so sanh
